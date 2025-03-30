268a30c1e35acb6db509105c86d4346490f7c068
Sanzhar Berkamalov <havocsolaran@gmail.com>
2025-03-30 14:11:25 +0500

    Added main menu screen

--- lib/main_menu_page.dart
+++ lib/main_menu_page.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'about_page.dart';
 import 'authors_page.dart';
+import 'scrollable_content.dart';
 
 class MainMenuPage extends StatelessWidget {
   const MainMenuPage({Key? key}) : super(key: key);
 
-  void _startGame(BuildContext context) {
+  void _openPage(BuildContext context, Widget page) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const AboutPage()),
-    );
-  }
-
-  void _showAuthors(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const AuthorsPage()),
+      MaterialPageRoute(builder: (context) => page),
     );
   }
 
@@ -30,7 +24,7 @@ class MainMenuPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () => _startGame(context),
+              onPressed: () => _openPage(context, const AboutPage()),
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size(200, 50),
                 textStyle: const TextStyle(fontSize: 18),
@@ -39,12 +33,21 @@ class MainMenuPage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => _showAuthors(context),
+              onPressed: () => _openPage(context, const AuthorsPage()),
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size(200, 50),
                 textStyle: const TextStyle(fontSize: 18),
               ),
               child: const Text('Authors'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _openPage(context, const ScrollableContent()),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(200, 50),
+                textStyle: const TextStyle(fontSize: 18),
+              ),
+              child: const Text('Game History'),
             ),
           ],
         ),
